85859b73d2f43b49638670769c778d67510d7996
Varun Chandran <me@varunchandran.com>
2013-10-28 01:55:47 -0500

    Created trainer

---
+++ src/main/java/com/varunchandran/blackjack/Deck.java
@@ -0,0 +1,27 @@
+package com.varunchandran.blackjack;
+
+public enum Deck {
+	Ace(-1), //
+	King(-1), //
+	Queen(-1), //
+	Jack(-1), //
+	Ten(-1), //
+	Nine(0), //
+	Eight(0), //
+	Seven(0), //
+	Six(1), //
+	Five(1), //
+	Four(1), //
+	Three(1), //
+	Two(1);
+
+	private int value;
+
+	private Deck(int value) {
+		this.value = value;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
