97f9636c37c88a06721f888ea5b99a9a16b0d52d
Varun Chandran <varunmc@gmail.com>
2017-01-22 13:01:57 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,9 +1,30 @@
-card-counting-trainer
-=====================
-A trainer to improve your Blackjack card counting.
+# card-counting-trainer
+_A trainer to improve your card counting_
 
-How to run:
------------
+![Screenshot](app.png)
 
+## Introduction
+Card Counting is particularly useful in Blackjack when trying to decide whether to Hit or Stand. As the cards flash 
+by, you maintain a running count that tells you whether the deck is loaded with low or high cards at any given point 
+in time. In the High-Low method of card counting, each card is given a value:
+
+- Cards from 2 - 6 have a value of +1
+- Cards 7 - 9 have a value of 0
+- Cards 10 - A have a value of -1
+
+The trainer walks you through a single deck at the desired cards per minute speed and you have to keep track of the 
+current score. As you get better, increase the speed to challenge yourself or add music and other distractions when
+to make it harder. The goal is to make card counting so effortless that it becomes second nature to you on the casino
+floor.  
+ 
+The on-screen guide lets you know how well you're doing:
+
+- Count: The number of cards counted
+- Score: The current score
+- Value: The value of each card
+
+## How To Run
 1. ```mvn clean package```
 2. ```java -jar target/card-counting-trainer-1.0.0.jar```
+
+###### Happy Training!
--- src/main/java/com/varunchandran/blackjack/Main.java
+++ src/main/java/com/varunchandran/blackjack/Main.java
@@ -10,7 +10,6 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Random;
-
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -21,155 +20,153 @@ import javax.swing.Timer;
 
 public final class Main extends JFrame implements ActionListener {
 
-	private static BufferedImage[] cards;
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
+  private static BufferedImage[] cards;
+  Random random;
+  private JButton actionButton;
+  private JPanel cardPanel;
+  private int count;
+  private JLabel countLabel;
+  private JTextField cpmField;
+  private int index = 0;
+  private int score;
+  private JLabel scoreLabel;
+  private Timer timer;
+  private boolean[] track;
+  private JLabel valueLabel;
+  private int value;
+  public Main() {
+    setTitle("Card Counting Trainer");
+    setLocationRelativeTo(null);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new BorderLayout());
 
-	private static final void loadCards() throws IOException {
-		cards = new BufferedImage[52];
-		for (int i = 0; i < 52; i++) {
-			cards[i] = ImageIO.read(Main.class.getResourceAsStream("/classic-cards/" + (i + 1) + ".png"));
-		}
-	}
+    add(getControlPanel(), BorderLayout.PAGE_START);
+    add(initImagePanel(), BorderLayout.CENTER);
+    add(getStatusPanel(), BorderLayout.LINE_END);
 
-	public static void main(String[] args) throws IOException {
-		loadCards();
-		new Main();
-	}
+    pack();
+    setVisible(true);
+  }
 
-	private JButton actionButton;
-	private JPanel cardPanel;
-	private int count;
-	private JLabel countLabel;
-	private JTextField cpmField;
-	private int index = 0;
-	Random random;
-	private int score;
-	private JLabel scoreLabel;
-	private Timer timer;
-	private boolean[] track;
-	private JLabel valueLabel;
-	private int value;
+  private static final void loadCards() throws IOException {
+    cards = new BufferedImage[52];
+    for (int i = 0; i < 52; i++) {
+      cards[i] = ImageIO.read(Main.class.getResourceAsStream("/classic-cards/" + (i + 1) + ".png"));
+    }
+  }
 
-	public Main() {
-		setTitle("Card Counting Trainer");
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setLayout(new BorderLayout());
+  public static void main(String[] args) throws IOException {
+    loadCards();
+    new Main();
+  }
 
-		add(getControlPanel(), BorderLayout.PAGE_START);
-		add(initImagePanel(), BorderLayout.CENTER);
-		add(getStatusPanel(), BorderLayout.LINE_END);
+  public void actionPerformed(ActionEvent e) {
+    if (e.getActionCommand() == null) {
+      doTimerTask();
+      return;
+    }
 
-		pack();
-		setVisible(true);
-	}
+    switch (e.getActionCommand()) {
+      case "Start":
+        start();
+        return;
 
-	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand() == null) {
-			doTimerTask();
-			return;
-		}
+      case "Stop":
+        stop();
+        return;
 
-		switch (e.getActionCommand()) {
-		case "Start":
-			start();
-			return;
+      default:
+        System.out.println(e.getActionCommand());
+    }
+  }
 
-		case "Stop":
-			stop();
-			return;
+  private void doTimerTask() {
+    if (count > 52) {
+      stop();
+      return;
+    }
 
-		default:
-			System.out.println(e.getActionCommand());
-		}
-	}
+    index = random.nextInt(52);
+    while (track[index] == true) {
+      index = random.nextInt(52);
+    }
 
-	private void doTimerTask() {
-		if (count > 52) {
-			stop();
-			return;
-		}
+    value = getCardValue(index);
+    score += value;
 
-		index = random.nextInt(52);
-		while (track[index] == true) {
-			index = random.nextInt(52);
-		}
+    countLabel.setText("Count: " + count);
+    valueLabel.setText("Value: " + value);
+    scoreLabel.setText("Score: " + score);
+    cardPanel.repaint();
 
-		value = getCardValue(index);
-		score += value;
+    track[index] = true;
+    count++;
+  }
 
-		countLabel.setText("Count: " + count);
-		valueLabel.setText("Value: " + value);
-		scoreLabel.setText("Score: " + score);
-		cardPanel.repaint();
+  private int getCardValue(int index) {
+    int ordinal = (int) Math.ceil(index / 4);
+    return Deck.values()[ordinal].getValue();
+  }
 
-		track[index] = true;
-		count++;
-	}
+  private JPanel getControlPanel() {
+    cpmField = new JTextField(5);
+    cpmField.setText("52");
+    actionButton = new JButton();
+    actionButton.setText("Start");
+    actionButton.addActionListener(this);
+    JLabel label = new JLabel("cards per minute");
+    JPanel panel = new JPanel();
+    panel.add(cpmField);
+    panel.add(label);
+    panel.add(actionButton);
+    return panel;
+  }
 
-	private int getCardValue(int index) {
-		int ordinal = (int) Math.ceil(index / 4);
-		return Deck.values()[ordinal].getValue();
-	}
+  private Component getStatusPanel() {
+    JPanel panel = new JPanel();
+    panel.setPreferredSize(new Dimension(80, 10));
+    GridLayout grid = new GridLayout(3, 1);
+    panel.setLayout(grid);
+    countLabel = new JLabel();
+    valueLabel = new JLabel();
+    scoreLabel = new JLabel();
+    panel.add(countLabel);
+    panel.add(scoreLabel);
+    panel.add(valueLabel);
+    return panel;
+  }
 
-	private JPanel getControlPanel() {
-		cpmField = new JTextField(5);
-		cpmField.setText("52");
-		actionButton = new JButton();
-		actionButton.setText("Start");
-		actionButton.addActionListener(this);
-		JLabel label = new JLabel("cards per minute");
-		JPanel panel = new JPanel();
-		panel.add(cpmField);
-		panel.add(label);
-		panel.add(actionButton);
-		return panel;
-	}
+  private Component initImagePanel() {
+    cardPanel = new JPanel() {
 
-	private Component getStatusPanel() {
-		JPanel panel = new JPanel();
-		panel.setPreferredSize(new Dimension(80, 10));
-		GridLayout grid = new GridLayout(3, 1);
-		panel.setLayout(grid);
-		countLabel = new JLabel();
-		valueLabel = new JLabel();
-		scoreLabel = new JLabel();
-		panel.add(countLabel);
-		panel.add(scoreLabel);
-		panel.add(valueLabel);
-		return panel;
-	}
+      private static final long serialVersionUID = 1L;
 
-	private Component initImagePanel() {
-		cardPanel = new JPanel() {
+      @Override
+      protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(cards[index], 100, 0, null);
+      }
+    };
+    cardPanel.setPreferredSize(new Dimension(0, 106));
+    return cardPanel;
+  }
 
-			private static final long serialVersionUID = 1L;
+  private void start() {
+    count = 1;
+    score = 0;
+    value = 0;
+    track = new boolean[52];
+    random = new Random();
+    int tick = (int) ((double) 60 / Integer.parseInt(cpmField.getText()) * 1000);
 
-			@Override
-			protected void paintComponent(Graphics g) {
-				super.paintComponent(g);
-				g.drawImage(cards[index], 100, 0, null);
-			}
-		};
-		cardPanel.setPreferredSize(new Dimension(0, 106));
-		return cardPanel;
-	}
+    timer = new Timer(tick, this);
+    timer.start();
+    actionButton.setText("Stop");
+  }
 
-	private void start() {
-		count = 1;
-		score = 0;
-		value = 0;
-		track = new boolean[52];
-		random = new Random();
-		int tick = (int) ((double) 60 / Integer.parseInt(cpmField.getText()) * 1000);
-
-		timer = new Timer(tick, this);
-		timer.start();
-		actionButton.setText("Stop");
-	}
-
-	private void stop() {
-		timer.stop();
-		actionButton.setText("Start");
-	}
+  private void stop() {
+    timer.stop();
+    actionButton.setText("Start");
+  }
 }
